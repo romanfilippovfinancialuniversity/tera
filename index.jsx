382e4189b447366a232fc6c3966d93bd20009362
romanfilippovfinancialuniversity <65555997+romanfilippovfinancialuniversity@users.noreply.github.com>
2021-06-21 10:33:50 +0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,17 @@
+class Hello extends React.Component {
+
+    render() {
+
+        return <h1>Hello, React</h1>;
+
+    }
+
+}
+
+ReactDOM.render(
+
+    <Hello></Hello>,
+
+    document.getElementById("app")
+
+)
